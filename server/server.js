d77b0ab2a21ffa9bc97bfad26a0ee243047a8042
Jorge Orta <123342325+jorgeorta01@users.noreply.github.com>
2024-12-07 11:59:46 -0600

    added Server folder

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const cors = require('cors');
+
+PORT=8080;
+
+// connect to db
+let db;
+(async () => {
+	db = await open({
+		// filename: 'friends.sqlite',   example example example example
+		driver: sqlite3.Database
+	});
+})();
+
+app = express();
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+app.use(cors());
+
+app.get ("/friends", async (req, res) => {
+	const result = await db.all ("SELECT * FROM friend");
+	res.json(result);
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
